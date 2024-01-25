e42f86066c4fed53dc6b2738332fe466c7baaca0
Juan Arnaboldi <nocto2014@gmail.com>
2024-01-25 16:13:48 -0300

    index fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+const root = document.getElementById('root');
+createRoot(root).render(
   <>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </>,
-
-  document.getElementById("root")
 );
